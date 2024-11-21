db796a48df529b31d47d6f43c16db29d93b90439
Sridhar <interiaengg@gmail.com>
2024-11-21 09:57:47 +0530

    Stable code Updated

--- src/test/java/FunctionLibrary/OtherFunctions.java
+++ src/test/java/FunctionLibrary/OtherFunctions.java
@@ -1,5 +1,7 @@
 package FunctionLibrary;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.List;
 
 
@@ -17,9 +19,15 @@ public class OtherFunctions extends AppUtils{
 		WebElement listContains=driver.findElement(By.xpath("//ul[@class='G43f7e']"));
 		List<WebElement> listOfItems=listContains.findElements(By.tagName("li"));
 		System.out.println("Total Number of Items: "+listOfItems.size());
+		File fi = new File("FileOutput/GoogleSearch.txt");
+		fi.createNewFile();
+		FileWriter fw = new FileWriter(fi);
+		fw.write("Google Search Results: \n");
 		for(int i=0; i<listOfItems.size(); i++) {
 			System.out.println(listOfItems.get(i).getText());
+			fw.write(listOfItems.get(i).getText()+"\n");
 		}
+		fw.close();
 	}
 
 }
