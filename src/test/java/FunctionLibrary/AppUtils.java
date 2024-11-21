53663a8ed4f480e944c31a18f76d16bbc83049d5
Sridhar <interiaengg@gmail.com>
2024-11-21 09:38:34 +0530

    Sample TestCase Created

---
+++ src/test/java/FunctionLibrary/AppUtils.java
@@ -0,0 +1,27 @@
+package FunctionLibrary;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+
+public class AppUtils {
+	
+	public static WebDriver driver;
+	@BeforeSuite
+	public void launchApp() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https:google.com");
+	}
+	
+	@AfterTest
+	public void closeApp() {
+		driver.quit();
+	}
+
+}
---
+++ src/test/java/FunctionLibrary/OtherFunctions.java
@@ -0,0 +1,23 @@
+package FunctionLibrary;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class OtherFunctions extends AppUtils{
+	
+	@Test
+	public void googleSearchTest() throws Throwable {
+		driver.findElement(By.className("gLFyf")).sendKeys("Java Basics");
+		Thread.sleep(1000);
+		WebElement listContains=driver.findElement(By.xpath("//ul[@class='G43f7e']"));
+		List<WebElement> listOfItems=listContains.findElements(By.tagName("li"));
+		System.out.println("Total Number of Items: "+listOfItems.size());
+		for(int i=0; i<listOfItems.size(); i++) {
+			System.out.println(listOfItems.get(i).getText());
+		}
+	}
+
+}
